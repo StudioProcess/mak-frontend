1250c7a0b33ac628eeceb22584b8a12d0c61873c
Martin Leopold <m@martinleopold.com>
2017-11-27 19:49:15 +0100

    Initial electron setup

---
+++ renderer.js
@@ -0,0 +1,12 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+
+const app = require("electron").remote.app;
+
+// document.addEventListener('keydown', function(e) {
+//   if (e.keyCode == 27) { // escape key maps to keycode `27`
+//     console.log("esc pressed");
+//     app.quit();
+//  }
+// });
