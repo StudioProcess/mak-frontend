41961d1052f5ae447c5a37da3c4a1db0d78207fe
Martin Leopold <m@martinleopold.com>
2017-12-06 14:46:21 +0100

    try earcut triangulation (pretty much fails)

--- renderer.js
+++ renderer.js
@@ -23,6 +23,7 @@ scene.add( camera ); // not needed?
 var renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize( W, H );
 document.body.appendChild( renderer.domElement );
+console.log(renderer);
 
 // Geometry
 let squareGeometry = new THREE.PlaneBufferGeometry( 100, 100 );
@@ -31,8 +32,12 @@ var square = new THREE.Mesh( squareGeometry, squareMaterial );
 scene.add( square );
 
 // Data
-let data = require('./assets/first_strokes.json');
-// console.log(data);
+let data = require('./assets/data/loop.json');
+
+
+function cleanEvents(eventData) {
+  return eventData.filter( (event) => event.type != 'stroke' || (event.node.x >= 0 && event.node.y >= 0) );
+}
 
 // Create Stroke array from event data
 // Stroke: { downTime, upTime, duration, timeDiff, nodes:[Node] }
@@ -50,27 +55,36 @@ function strokesFromEvents(eventData) {
       }
     } else if (event.type == 'stroke') {
       acc[acc.length-1].nodes.push(event.node);
+      // TODO: add absolute time
     }
     return acc;
   }, []);
 }
+
+// TODO: simplify strokes
 
 // Create vertex array for rendering
 function vertexArrayFromStrokes(strokes) {
   const offset = 5;
   const scale = 10;
   
+  // let initialData = [-W/2, H/2, -W/2, -H/2, W/2, -H/2, W/2, H/2, -W/2, H/2,];
+  let initialData = [];
   let vertexData = strokes.reduce((acc, stroke) => {
     let strokeVertices = stroke.nodes.reduce((acc, node) => {
       acc.push( -W/2 + (node.x-offset)*scale, H/2 - (node.y-offset)*scale ); // 
       return acc;
     }, []);
     return acc.concat(strokeVertices);
-  }, []);
+  }, initialData);
+  
   
   return new Float32Array(vertexData);
 }
 
+
+data = cleanEvents(data);
+console.log(data);
 let strokes = strokesFromEvents(data);
 // console.log(strokes);
 let vertexData = vertexArrayFromStrokes(strokes);
@@ -83,7 +97,25 @@ let line = new THREE.Line( lineGeometry, lineMaterial );
 scene.add( line );
 
 
-// Render loop
+// triangulation
+const earcut = require('earcut');
+let triangles = earcut(vertexData);
+let indexData = new Int32Array(triangles);
+// console.log(indexData);
+
+let tessGeometry = new THREE.BufferGeometry();
+tessGeometry.addAttribute( 'position', new THREE.BufferAttribute(vertexData, 2) );
+tessGeometry.setIndex( new THREE.BufferAttribute(indexData, 1) );
+let tessMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true });
+let tessMesh = new THREE.Mesh( tessGeometry, tessMaterial );
+scene.add(tessMesh);
+
+
+
+/* 
+  RENDER LOOP
+ */
+
 let prevTime = 0.0;
 let elapsedTime = 0.0;
 
@@ -95,7 +127,7 @@ function animate(time) {
   stats.begin();
   renderer.render( scene, camera );
   square.rotation.z += 0.01;
-  line.rotation.z -= 0.01;
+  // line.rotation.z -= 0.01;
   stats.end();
   
   requestAnimationFrame( animate );
